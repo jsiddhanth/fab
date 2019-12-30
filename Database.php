b790cfe0f6528768c3bf0f273af8b42e4f4b9926
jsiddhanth <55252679+jsiddhanth@users.noreply.github.com>
2019-12-30 08:26:21 +0530

    Add files via upload

---
+++ Database.php
@@ -0,0 +1,33 @@
+<?php
+
+    class Database
+    {
+        private  $host     = 'localhost';
+        private  $db_name  = 'myblog';
+        private  $username = 'root';
+        private  $password = '';
+        private  $conn;
+
+        //DB connect
+        public function connect()
+        {
+            $this->conn = NULL;
+
+            try
+            {
+                $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,
+                $this->username,$this->password);
+
+                $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            }
+
+            catch(PDOException $e)
+            {
+                echo 'Connection error : ' . $e->getMessage();
+            }
+            
+            return $this->conn;
+            
+        }
+
+    }
